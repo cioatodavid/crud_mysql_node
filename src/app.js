3ac095288bf0797e60f812badb2331388a4b35bd
david cioato <cioatodavid@gmail.com>
2022-06-06 14:25:38 -0300

    update gitignore e app.js

--- src/app.js
+++ src/app.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server running on: http://localhost:${port}`))
