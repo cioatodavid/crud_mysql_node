5f1b6fd6d780569c7254fcc97b1e7a1e71eeafdc
LariiiLopes <lariane.lopesc@gmail.com>
2022-06-15 09:18:48 -0300

    add controller/routes search

--- src/controllers/replacementPartController.js
+++ src/controllers/replacementPartController.js
@@ -38,6 +38,80 @@ class ReplacementPartController {
         })
     }
 
+    static searchReplacementPartByWidth(req, res) {
+        dbConnection.query(`SEARCH INTO replacement_part_for_machine (MACHINE_WIDTH, REPLACEMENT_PART_WIDHT) values (
+                ${req.body.MACHINE_WIDHT},
+                ${req.body.REPLACEMENT_PART_WIDHT}
+                )`, (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({
+                    message: 'Error searching replacement part connection'
+                })
+                return
+            }
+            res.json ({
+                message: 'Replacement part connection search sucessfully'
+            })
+        })
+    }
+
+    static searchReplacementPartByHeight(req, res) {
+        dbConnection.query(`SEARCH INTO replacement_part_for_machine (MACHINE_HEIGHT, REPLACEMENT_PART_HEIGHT) values (
+                ${req.body.MACHINE_HEIGHT},
+                ${req.body.REPLACEMENT_PART_HEIGHT}
+                )`, (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({
+                    message: 'Error searching replacement part connection'
+                })
+                return
+            }
+            res.json ({
+                message: 'Replacement part connection search sucessfully'
+            })
+        })
+    }
+
+    static searchReplacementPartByDepth(req, res) {
+        dbConnection.query(`SEARCH INTO replacement_part_for_machine (MACHINE_DEPTH, REPLACEMENT_PART_DEPTH) values (
+                ${req.body.MACHINE_DEPTH},
+                ${req.body.REPLACEMENT_PART_DEPTH}
+                )`, (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({
+                    message: 'Error searching replacement part connection'
+                })
+                return
+            }
+            res.json ({
+                message: 'Replacement part connection search sucessfully'
+            })
+        })
+    }
+
+    
+    static searchReplacementPartByWeight(req, res) {
+        dbConnection.query(`SEARCH INTO replacement_part_for_machine (MACHINE_WEIGTH, REPLACEMENT_PART_WEIGHT) values (
+                ${req.body.MACHINE_WEIGHT},
+                ${req.body.REPLACEMENT_PART_WEIGHT}
+                )`, (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({
+                    message: 'Error searching replacement part connection'
+                })
+                return
+            }
+            res.json ({
+                message: 'Replacement part connection search sucessfully'
+            })
+        })
+    }
+
+
     static deleteReplacementPart(req, res) {
         dbConnection.query(`DELETE FROM replacement_part_for_machine WHERE MACHINE_ID = "${req.params.macID}" and REPLACEMENT_PART_ID = ${req.params.repID} `, (err, rows) => {
             if (err) {
--- src/routes/replacementPartRoutes.js
+++ src/routes/replacementPartRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 router    
     .post('/replacement', ReplacementPartController.insertReplacementPart)
     .get('/replacement',ReplacementPartController.searchReplacementPart)
+    .get('/replacement/:width', ReplacementPartController.searchReplacementPartByWidth)
+    .get('/replacement/:height', ReplacementPartController.searchReplacementPartByHeight)
+    .get('/replacement/:depth', ReplacementPartController.searchReplacementPartByDepth)
+    .get('/replacement/:weight', ReplacementPartController.searchReplacementPartByWeight)
     .delete('/replacement/:macID/:repID', ReplacementPartController.deleteReplacementPart)
 
     
