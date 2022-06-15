d7c6909cdb49af87b2bedf87e6e39022e89b4159
david cioato <cioatodavid@gmail.com>
2022-06-15 08:55:17 -0300

    update

--- src/controllers/machinePartController.js
+++ src/controllers/machinePartController.js
@@ -1,33 +1,33 @@
 const dbConnection = require('../config/dbConnect')
 
 class MachinePartController {
-    static getMachinePart (req, res) {
-        dbConnection.query ('SELECT * FROM replacement_part', (err, rows) => {
+    static getMachinePart(req, res) {
+        dbConnection.query('SELECT * FROM replacement_part', (err, rows) => {
             if (err) {
                 res.status(500).json({
                     message: 'Error getting machines part'
                 })
-                console.log (err)
+                console.log(err)
             } else {
                 res.json(rows)
             }
         })
     }
 
-    static getMachinePartByPage (req, res){
+    static getMachinePartByPage(req, res) {
         const page = req.params.page
         const limit = 5
-        const offset = (page -1) * limit
+        const offset = (page - 1) * limit
         dbConnection.query('SELECT * FROM replacement_part LIMIT ? OFFSET ?', [limit, offset], (err, rows) => {
             if (err) {
                 res.status(500).json({
                     message: 'Error getting machines part'
                 })
-                console.log(err)     
+                console.log(err)
             } else {
-                res.json ({
-                    nextPage: (number(page) +1),
-                    prevPage: (page -1) > 0 ? (Number(page) -1) : 1,
+                res.json({
+                    nextPage: (number(page) + 1),
+                    prevPage: (page - 1) > 0 ? (Number(page) - 1) : 1,
                     currPage: rows
                 })
             }
@@ -37,20 +37,20 @@ class MachinePartController {
     static getMachinePartById(req, res) {
         const id = req.params.id
         dbConnection.query('SELECT * FROM replacement_part WHERE id = ?'), id,
-        (err, rows) => {
-            if (err) {
-                res.status(500).json({
-                    message: 'Error getting machine part'
-                })
-                console.log(err)
-            } else {
-                res.json(rows)
+            (err, rows) => {
+                if (err) {
+                    res.status(500).json({
+                        message: 'Error getting machine part'
+                    })
+                    console.log(err)
+                } else {
+                    res.json(rows)
 
+                }
             }
-        }
     }
 
-    static insertMachinePart (req, res) {
+    static insertMachinePart(req, res) {
         dbConnection.query(`INSERT INTO replacement_part (SERIAL_NUMBER, WIDTH, HEIGHT, DEPTH, WEIGHT) values (
                 ${req.body.SERIAL_NUMBER},
                 ${req.body.WIDTH},
@@ -60,19 +60,19 @@ class MachinePartController {
                 )`, (err, rows) => {
             if (err) {
                 console.log(err)
-                res.status(500).json ({
+                res.status(500).json({
                     message: 'Error inserting machine part'
                 })
                 return
-            }    
+            }
             res.json({
                 message: 'Machine part inserted sucessfully'
-            })    
+            })
         })
-        
+
     }
 
-    static updateMachinePart(req, res){
+    static updateMachinePart(req, res) {
         const id = req.params.id
         dbConnection.query('UPDATE machine SET ? WHERE id = ?', [req.body, id], (err, rows) => {
             if (err) {
@@ -82,7 +82,7 @@ class MachinePartController {
                 })
                 return
             }
-            res.json ({
+            res.json({
                 message: 'Machine part update sucessfully',
                 update: req.body
             })
@@ -105,13 +105,35 @@ class MachinePartController {
         })
     }
 
+    static searchMachinePartWithPage(req, res) {
+        const query = req.query.search
+        const value = req.query.value
+        const page = number(req.query.page)
+        const limit = 5
+        const offset = (page - 1) * limit
+        dbConnection.query('SELECT * FROM replacement_part WHERE ${query} LIKE ${value} LIMIT ? OFFSET ?', [limit, offset], (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({
+                    message: 'Error searching machine part'
+                })
+                return
+            }
+            res.json({
+                nextPage: (Number(page) + 1),
+                prevPage: (page - 1) > 0 ? (Number(page) - 1) : 1,
+                currPage: rows
+            })
+        })
+    }
+
     static searchMachinePartWithPage (req, res){
         const query = req.query.search
         const value = req.query.value
         const page = number(req.query.page)
         const limit = 5
         const offset = (page -1) * limit
-        dbConnection.query('SELECT * FROM replacement_part WHERE ${query} LIKE ${value} LIMIT ? OFFSET ?', [limit, offset], (err, rows) => {
+        dbConnection.query(`SELECT * FROM replacement_part WHERE ${query} LIKE ${value} LIMIT ? OFFSET ?`, [limit, offset], (err, rows) => {
             if (err) {
                 console.log(err)
                 res.status(500).json({
--- src/routes/machineRoutes.js
+++ src/routes/machineRoutes.js
@@ -8,7 +8,6 @@ router
     .post('/machine', MachineController.insertMachine)
     .put('/machine/:id', MachineController.updateMachine)
     .delete('/machine/:id', MachineController.deleteMachine)
-    //.post('/machine/findby/', MachineController.searchMachine) //show all results
     .post('/machine/findby/page/', MachineController.searchMachineWithPage)
     .get('/machine/page/:page', MachineController.getMachinesByPage)
     
